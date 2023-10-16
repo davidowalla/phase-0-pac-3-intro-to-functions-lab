e9b9595af91c6b4d0e742b97e077f5b2822bc651
david@thekayappo.com <davidowalla@Davids-MacBook-Pro.local>
2023-10-16 21:40:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(_string) {
+    console.log ('HELLO');
+}
+
+function logWhisper(_string) {
+    console.log ('hello');
+}
+
+function logShout(_string) {
+    console.log ('HELLO');
+}
+
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello') {
+        return 'I can\'t hear you!';
+    }
+    else if (string === 'HELLO') {
+        return 'YES INDEED!';
+    }
+    else if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
+
+
